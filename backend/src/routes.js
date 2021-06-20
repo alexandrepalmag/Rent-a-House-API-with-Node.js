34700118d3bfd090fa90d638632c98c17c11fc62
alexandrepalmag <alepalmag85ti@gmail.com>
2021-06-20 02:08:30 -0300

    Creation of model and controller to register houses. The image is stored in the application's uploads folder. Thumbnail stores only the path of the image. Installation of multer library to work with image.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from './config/upload';
 
 import SessionController from './controllers/SessionController';
+import HouseController from './controllers/HouseController';
 
 const routes = new Router();
+const upload = multer(uploadConfig)
 
 routes.post('/sessions', SessionController.store); // to login
+
+routes.post('/houses', upload.single('thumbnail'), HouseController.store);//to register houses in application
 
 export default routes;
