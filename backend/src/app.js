fdba150e2971ce79f82c56d25c0479a7719bbf08
alexandrepalmag <alepalmag85ti@gmail.com>
2021-06-19 22:43:14 -0300

    Setting up connection with MogoDB. Installation of Mongoose.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App {
 
     constructor() {
+
         this.server = express();
+        mongoose.connect('mongodb+srv://renthouse:renthouse@renthouse.1b6yj.mongodb.net/renthouse?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
         this.middlewares();
         this.routes();
     }
